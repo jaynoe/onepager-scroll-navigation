77d2861839a06fb4e20dfb512c4f25e641a61a2d
Jannik <janniknoelke@protonmail.com>
2018-10-12 13:43:50 +0200

    First upload

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# onepager-scroll-navigation
+# jQuery Onepager-Scroll-Navigation
+Example: _Soon_
+
+## Requirements
+* jQuery (tested with v3.3.1)
+
+## HTML structure
+```
+<nav class="navigation">
+<ul>
+    <li>
+        <a href="#area1" data-area="area1">Area 1</a>
+        <a href="#area2" data-area="area2">Area 2</a>
+        <a href="#area3" data-area="area3">Area 3</a>
+        <a href="#area4" data-area="area4">Area 4</a>
+    </li>
+</ul>
+</nav>
+
+<div class="container" id="area1"><!-- Content --></div>
+<div class="container" id="area2"><!-- Content --></div>
+<div class="container" id="area3"><!-- Content --></div>
+<div class="container" id="area4"><!-- Content --></div>
+```
+
+## JavaScript
+```
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script>$(".navigation").onepagerScrollNavigation();</script>
+```
+
+## Options
+* **duration**<br>Scroll speed in ms (default: 1000)
+* **activeClass**<br>Class for the active navigation link (default: "active")
+* **containerClass**<br>Your area class (default: ".container")
+* **offsetTop**<br>If you have a fixed header you have to enter its height here (default: 0)
---
+++ jquery.onepager-scroll-navigation.js
@@ -0,0 +1,53 @@
+/**
+ * created by @jaynoe
+ * https://github.com/jaynoe
+ *
+ * Plugin: jquery-onepager-navigation
+ * https://github.com/jaynoe/onepager-scroll-navigation
+ */
+
+;(function($){
+
+    $.fn.onepagerScrollNavigation = function(options){
+
+        // default options
+        options = $.extend({
+            duration: 1000,
+            activeClass : "active",
+            containerClass: ".container",
+            offsetTop: 0
+        }, options);
+
+        var $navigation = $(this);
+        var $link = $navigation.find("a");
+        var offset = options.offsetTop;
+
+        // add active class to first link
+        $navigation.find("li:first-child a").addClass(options.activeClass);
+
+        // trigger click function
+        $link.click(function(e) {
+            // prevent "normal" behavior
+            e.preventDefault();
+            // get current hash
+            var hash = $(this).prop("hash");
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top-offset
+            }, options.duration);
+        });
+
+        $(window).scroll(function() {
+            var pos = $(window).scrollTop();
+            $(options.containerClass).each(function() {
+                var section = $(this).offset().top-offset;
+                var id = $(this).attr("id");
+                if(pos >= section) {
+                    $navigation.find("a[data-area]").removeClass(options.activeClass);
+                    $navigation.find("a[data-area='"+id+"']").addClass(options.activeClass);
+                }
+            });
+        });
+
+    };
+
+})(jQuery);
